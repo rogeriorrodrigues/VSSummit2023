e483bccf8bf24ab8cd656e21b9c9cb095248471c
Rogério Rodrigues <RRODRIGUES@microsoft.com>
2022-08-31 16:36:45 -0300

    add solução final web

---
+++ DesafioWebCsharp/Controllers/CardsController.cs
@@ -0,0 +1,72 @@
+﻿using DesafioWebCsharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using X.PagedList;//namespace do pagedlist core
+
+namespace DesafioWebCsharp.Controllers
+{
+
+    public class CardsController : Controller
+    {
+        private readonly string ENDPOINT = "https://api.magicthegathering.io/v1/cards";
+        private readonly HttpClient httpClient = null;
+        private static List<Card> _cards = null;
+
+        public CardsController() { 
+        
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ENDPOINT);
+
+        }
+        public async Task<IActionResult> Index(int? page)
+        {
+            //config para o pagged
+            var itemsByPage = 10;
+            var currentPage = page ?? 1;
+
+            try
+            {
+                MagicCard cardList = null;
+                _cards = new List<Card>();
+
+                HttpResponseMessage response = await httpClient.GetAsync(ENDPOINT);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    cardList = JsonConvert.DeserializeObject<MagicCard>(content);
+                    
+
+                    foreach (var card in cardList.cards)
+                    { 
+                        _cards.Add(card);
+                    }
+
+                }
+                else
+                {
+                    ModelState.AddModelError(null, "Erro ao realizar a chamada");
+                }
+                //Com paginação
+                return View(await cardList.cards.ToPagedListAsync(currentPage, itemsByPage));
+                //Sem Paginação
+                //return View(cardList.cards);
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                throw ex;
+            }
+           
+        }
+
+        //Método para os detalhes
+        [HttpGet]
+        public IActionResult Details(string id)
+        {
+            Card card = _cards.FirstOrDefault(c => c.id.Equals(id));
+            return View(card);
+             
+        }
+    }
+}
---
+++ DesafioWebCsharp/Models/MagicCard.cs
@@ -0,0 +1,79 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+namespace DesafioWebCsharp.Models
+{
+
+    public class MagicCard
+    {
+        public List<Card> cards { get; set; }
+    }
+
+    public class Card
+    {
+        public string name { get; set; }
+        public string manaCost { get; set; }
+        public double cmc { get; set; }
+        public List<string> colors { get; set; }
+        public List<string> colorIdentity { get; set; }
+        public string type { get; set; }
+        public List<string> supertypes { get; set; }
+        public List<string> types { get; set; }
+        public List<string> subtypes { get; set; }
+        public string rarity { get; set; }
+        public string set { get; set; }
+        public string setName { get; set; }
+        public string text { get; set; }
+        public string artist { get; set; }
+        public string number { get; set; }
+        public string layout { get; set; }
+        public string multiverseid { get; set; }
+        public string imageUrl { get; set; }
+        public string loyalty { get; set; }
+        public List<string> variations { get; set; }
+        public List<Ruling> rulings { get; set; }
+        public List<ForeignName> foreignNames { get; set; }
+        public List<string> printings { get; set; }
+        public string originalText { get; set; }
+        public string originalType { get; set; }
+        public List<Legality> legalities { get; set; }
+        public string id { get; set; }
+        public string flavor { get; set; }
+        public string power { get; set; }
+        public string toughness { get; set; }
+        public string watermark { get; set; }
+       
+    }
+
+    public class ForeignName
+    {
+        public string name { get; set; }
+        public string text { get; set; }
+        public string type { get; set; }
+        public object flavor { get; set; }
+        public string imageUrl { get; set; }
+        public string language { get; set; }
+        public object multiverseid { get; set; }
+        public string faceName { get; set; }
+    }
+
+    public class Legality
+    {
+        public string format { get; set; }
+        public string legality { get; set; }
+    }
+
+    public class Root
+    {
+        
+    }
+
+    public class Ruling
+    {
+        public string date { get; set; }
+        public string text { get; set; }
+    }
+
+    
+}
