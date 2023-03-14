b41f1441b7a5daf5ae1647e4110b26ce518e7446
Rogério Rodrigues <rogeriorodrigues@outlook.com>
2023-03-14 14:24:38 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+string teste = "zzxu7gnfb5xpuiqvjdk4vqjmfecxbqymym6aenii5g3zqygj7ahq";
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
